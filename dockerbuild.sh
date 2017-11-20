434fce87592beb5a71730bd6eae13f43451b40f5
cvemula1 <cvemula4511@ucumberlands.edu>
2017-11-20 13:47:23 -0600

    Update dockerbuild.sh

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,10 +1,9 @@
 export DOCKER_ID_USER="chennaraov"
-export DOCKER_IMAGE="springboot"
+export DOCKER_IMAGE="tomcat-web"
 chmod 700 dockerbuild.sh
 mvn clean install
-docker build -t springboot .
-docker tag springboot:latest $DOCKER_ID_USER/$DOCKER_IMAGE:latest
+docker build -t tomcat-web .
+docker tag tomcat-web:latest $DOCKER_ID_USER/$DOCKER_IMAGE:latest
 docker login -u $DOCKER_ID_USER -p $DOCKER_PW
 docker push $DOCKER_ID_USER/$DOCKER_IMAGE:latest
 docker images -qa | xargs docker rmi -f
-rm -rf targt
